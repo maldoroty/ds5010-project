76503e9d0acc38129c72052db3835dbd54080d06
Michael Aldoroty <michaelaldoroty@gmail.com>
2022-08-03 11:17:38 -0400

    starting the node class

--- decision_tree.py
+++ decision_tree.py
@@ -2,4 +2,18 @@
 DS5010 Decision Tree Project
 '''
 
-import pandas as pd
+#import pandas as pd
+
+class Node:
+
+    def __init__(self, data, prev = None, next = None):
+        self.data = data
+        self.prev = prev
+        self.next = next
+    
+    def get_data(self):
+        return self.data
+
+if __name__ == "__main__":
+    x = Node(20)
+    print(x.get_data())
