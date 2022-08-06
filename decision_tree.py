9648336c909e2feb373f4525d2db1997adf073fd
Michael Aldoroty <michaelaldoroty@gmail.com>
2022-08-06 16:22:15 -0400

    importing counter for the leaf node value

--- decision_tree.py
+++ decision_tree.py
@@ -3,6 +3,7 @@ DS5010 Decision Tree Project
 '''
 import pandas as pd
 import numpy as np
+from collections import Counter
 
 class Node:
     
@@ -21,32 +22,32 @@ class Node:
 
 class DecisionTree:
 
-    def __init__(self, min_split = 2, max_depth = None, total_features = None):
+    def __init__(self, min_split = 2, max_depth = 10, total_features = None):
         '''initiliazing variables that control the decision tree builder'''
         self.root = None
         self.min_split = min_split
-        max_depth = max_depth
+        self.max_depth = max_depth
     
     def start_tree(self, data, col_labels):
         '''starting the tree at the root'''
-        self.total_features = data.shape[1]
         self.root = self.build_tree(data, col_labels)
     
-    def build_tree(self, data, col, depth):
+    def build_tree(self, data, col_labels, depth = 0):
         '''building the tree'''
-        samples, features = np.shape(data)
-        diff_labels = np.df['labels'].nunique()
+        samples, features = data.shape()
+        diff_labels = len(np.df['labels'].nunique())
         # stopping condition
         if diff_labels <= 1 or depth >= self.max_depth:
             return Node(label = self.count_leaf(data))
         # need to add traversal of each column element to find best information gain to build tree recurisvely
-        best_split = self.best_split(data, col)
+        best_split = self.best_split(data, col_labels)
         # need to split the data here
         # return decision node from dictionary best_split to fill out arguments
         return Node()
     
-    def best_split(data, col):
+    def best_split(self, data, col):
         pass
 
     def count_leaf(self, col_labels):
-        return max(list(col_labels), key = col_labels.count)
+        count = Counter(col_labels)
+        return count.most_common(1)[0][0]
