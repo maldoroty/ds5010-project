ad9f00c555a9db12d5e47f19b321bcb0842be6e2
Michael Aldoroty <michaelaldoroty@gmail.com>
2022-08-05 22:24:28 -0400

    starting the DecisionTree class

--- decision_tree.py
+++ decision_tree.py
@@ -14,3 +14,27 @@ class Node:
         self.right = right
         self.gini_index = gini_index
         self.label = label
+
+class DecisionTree:
+
+    def __init__(self):
+        pass
+
+    def build_tree(self, data):
+        
+        diff_labels = np.df['labels'].nunique()
+        if diff_labels > 1:
+            best_split = self.best_split(data)
+            left, right = self.build_tree.split(data)
+            return Node(best_split["category"], best_split["split_value"], left, right, best_split["gini_index"])
+        
+        return Node(label = self.count_leaf(data))
+
+    def best_split(data):
+        pass
+
+    def split(data):
+        pass
+    
+    def count_leaf(data):
+        pass
